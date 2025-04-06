d36aef273d2d906369cba7785f949acb5c8ccec1
Ahmed Torki <ahmedtorki@Ahmeds-MacBook-Air.local>
2025-04-06 05:39:51 -0700

    locators

--- selenium_test.py
+++ selenium_test.py
@@ -8,7 +8,12 @@ driver.get("http://www.python.org")
 print(driver.title)
 
 search_bar = driver.find_element(By.ID, "id-search-field")
-search_bar.send_keys("asyncio")
-
+go_button = driver.find_element(By.NAME, "submit")
 time.sleep(2)
+
+search_bar.send_keys("asyncio")
+go_button.click()
+time.sleep(2)
+
 driver.quit()
+
